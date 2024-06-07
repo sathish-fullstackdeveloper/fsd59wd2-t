84af881d8385980dc0ff6496a9f2d09554573ef9
githubscientist <sathish.datascientist@gmail.com>
2024-06-07 19:47:08 +0530

    working with lists

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Rendering: inserting the HTML code into the DOM
+
+## Doubts:
+
+- Usage of map function in react
+- destructuring in react
+- hooks in react: useState, useEffect
+- cloning repo from github to local
+- react router dom
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 const App = () => {
-  return <h1>Hello FSD59WD2-T Students!!</h1>
+
+  const favourites = ['React', 'Node', 'MongoDB', 'Express', 'JavaScript'];
+
+  // [5, 4, 7, 7, 10]
+
+  // create an empty array to store the list of favourite items as JSX elements or React elements
+  const favouriteList = [];
+
+  // loop through the favourites array
+  for (let index = 0; index < favourites.length; index++){
+    favouriteList.push(<li>{favourites[index]}</li>);
+  }
+
+  console.log(favouriteList);
+
+  return (
+    <>
+      <h1>My Favourite List</h1>
+      <ul>
+        {favouriteList}
+      </ul>
+    </>
+  )
 }
 
 export default App;
